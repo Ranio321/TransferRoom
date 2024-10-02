497a98c218a99124c7095c5ae9dd9feee0f93eb5
Paweł Raniowski <pawel.raniowski@euvic.pl>
2024-10-02 14:37:07 +0200

    Add scroll to top

--- client/src/common/layout/Root.tsx
+++ client/src/common/layout/Root.tsx
@@ -1,14 +1,13 @@
 import { Box } from "@mui/material";
 import { NavBar } from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 export const Root = () => {
   return (
     <Box>
       <NavBar />
-      <Box>
-        <Outlet />
-      </Box>
+      <Outlet />
+      <ScrollRestoration />
     </Box>
   );
 };
